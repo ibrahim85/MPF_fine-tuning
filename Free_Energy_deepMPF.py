b6fb691b0070af40898e4a50e6cb2d5640bb6b1e
liuzuozhu <lcowen.hn@hotmail.com>
2017-01-11 21:35:41 +0800

    numpy mpf with manually gradients

---
+++ Free_Energy_deepMPF.py
@@ -0,0 +1,308 @@
+'''
+This is the deep MPF. We implement the MPF-EM algorithm here for training RBMs.
+Step 1: Binarize the input data, initialize weight and bias
+Step 2: Feedforward and sampling to get the hidden activations
+Step 3: Train the MPF to find the better weights and bias
+Step 4: repeat Step 2 and 3 until weight converge.
+Step 5: Sanity check, conduct generating tasks
+'''
+
+
+from dmpf_optimizer import *
+from sklearn import preprocessing
+import numpy as np
+import gzip
+import timeit, pickle, sys, math
+import theano
+import theano.tensor as T
+import Image
+import copy
+import os
+from free_energy_dmpf import *
+
+from utils import tile_raster_images
+
+
+
+def get_mpf_params(visible_units, hidden_units):
+
+    '''
+    :param visible_units: number of units in the visible layer
+    :param hidden_units: number of units ni the hidden layer
+    :return: The well structured MPF weight matrix
+    The MPF weight matrix is of the form:
+    [0,   W,
+     W.T, 0]
+    '''
+    numpy_rng = np.random.RandomState(555555)
+
+    W = numpy_rng.randn(visible_units,hidden_units)/np.sqrt(visible_units*hidden_units)
+
+   # W = np.random.uniform(low=-1, high=1,size = (visible_units,hidden_units))
+
+    W_up = np.concatenate((np.zeros((visible_units,visible_units)), W), axis = 1)
+
+    W_down = np.concatenate((W.T,np.zeros((hidden_units,hidden_units))), axis = 1 )
+
+    W = np.concatenate((W_up,W_down), axis = 0)
+
+    print(W.shape)
+
+    return W
+
+
+
+def mpf_em(dataset,hidden_units,dynamic=False):
+
+
+    ################################################################
+    ################## Loading the Data        #####################
+    ################################################################
+
+    if dataset is None:
+        dataset = 'mnist.pkl.gz'
+        f = gzip.open(dataset, 'rb')
+        train_set, valid_set, test_set = pickle.load(f,encoding="bytes")
+        f.close()
+
+    else:
+        f = gzip.open(dataset, 'rb')
+        train_set, valid_set, test_set = pickle.load(dataset,encoding="bytes")
+        f.close()
+
+
+    binarizer = preprocessing.Binarizer(threshold=0.5)
+    data =  binarizer.transform(train_set[0])
+    # k = data[:1,:]
+    # print(k)
+    # print(train_set[0][:1,:])
+
+    #data = np.load('gibbs_samples.npy')
+
+
+    ################################################################
+    ##################  Initialize Parameters  #####################
+    ################################################################
+
+    #visible_units = train_set[0].shape[1]
+    visible_units = data.shape[1]
+
+    num_units = visible_units + hidden_units
+
+    W = get_mpf_params(visible_units, hidden_units)
+
+    # np.save('rbm_W.npy',W)
+    # bias can be intialized in the MPF_optimizer class
+
+    ################################################################
+    ##################  Train MPF              #####################
+    ################################################################
+
+    # In one round, we feedfoward the and get the samples,
+    # Compute the probability of each data samples,
+    # call the minimum probability flow objective function
+
+    epsilon = 0.01
+    learning_rate = 0.002
+    index = T.lscalar()    # index to a mini batch
+    x = T.matrix('x')
+    batch_sz = 20
+
+    mpf_optimizer = dmpf_optimizer(epsilon=epsilon, num_units = num_units, W = W, b = None,
+                 input = x,batch_sz =batch_sz)
+
+
+    n_train_batches = data.shape[0]//batch_sz
+
+    data  = theano.shared(value=np.asarray(data, dtype=theano.config.floatX),
+                                  name = 'train',borrow = True)
+
+    cost,updates = mpf_optimizer.get_dmpf_cost(learning_rate= learning_rate,
+                                               visible_units=visible_units,hidden_units=hidden_units)
+
+    train_mpf = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x: data[index * batch_sz: (index + 1) * batch_sz],
+        },
+        #on_unused_input='warn',
+    )
+
+    training_epochs = 200
+
+    start_time = timeit.default_timer()
+
+    mean_epoch_error = []
+
+    for epoch in range(training_epochs):
+
+        # go through the training set
+        mean_cost = []
+        mean_batch_error = []
+        norm_batch_error = []
+
+
+        for batch_index in range(n_train_batches):
+            weight = mpf_optimizer.W.get_value(borrow = True)
+            bia = mpf_optimizer.b.get_value(borrow = True)
+            mean_cost += [train_mpf(batch_index)]
+
+        mean_epoch_error += [np.mean(mean_cost)]
+        print('The cost for dmpf in epoch %d is %f'% (epoch,np.mean(mean_cost)))
+            # print(mean_cost)
+            # error_W = np.sum((W_init - mpf_optimizer.W.get_value(borrow=True))**2)
+            # error_bias = np.sum((bia - mpf_optimizer.b.get_value(borrow= True))**2)
+            #
+            # #print(error_bias)
+            #
+            # error = error_bias + error_W
+            #
+            # mean_batch_error += [error_W/(2*hidden_units*visible_units)]
+
+            # norm_batch_error += [np.sum(( W/(np.sum(W**2)) -
+            # mpf_optimizer.W.get_value(borrow=True)/(np.sum(mpf_optimizer.W.get_value(borrow=True)**2)) )**2 )]
+
+
+        if epoch % 5 == 0 :
+            image = Image.fromarray(
+            tile_raster_images(
+                X=(mpf_optimizer.W.get_value(borrow = True)[:visible_units,visible_units:]).T,
+                img_shape=(28, 28),
+                tile_shape=(15, 15),
+                tile_spacing=(1, 1)
+            )
+            )
+            np.save('W_epoch_'+str(epoch)+'.npy',
+                    (mpf_optimizer.W.get_value(borrow = True)[:visible_units,visible_units:]).T)
+            image.show()
+            image.save('filters_at_epoch_%i.png' % epoch)
+
+        # if (epoch +1) % 100 == 0:
+        #     learning_rate = learning_rate / 10
+        #mean_epoch_error += [np.mean((mean_batch_error))]
+
+        # print(mean_cost)
+
+        # print(mean_epoch_error[-1])
+        #
+        # if epoch > 2 and mean_epoch_error[-1] > mean_epoch_error[-2]:
+        #     print('Ending epoch is %d .' % epoch)
+        #     break
+        # norm_epoch_error += [np.mean(np.sqrt(norm_batch_error))]
+        #
+        # print('Training epoch %d, cost is %f' % (epoch, np.mean(mean_cost) ) )
+
+    end_time = timeit.default_timer()
+
+    pretraining_time = (end_time - start_time)
+
+    print ('Training took %f minutes' % (pretraining_time / 60.))
+
+
+    ################################################################
+    ##################  Sampling               #####################
+    ################################################################
+
+    n_chains = 20
+    n_samples = 10
+    rng = np.random.RandomState(123)
+    test_set_x = test_set[0]
+    number_of_test_samples = test_set_x.shape[0]
+    test_set_x = theano.shared( value = np.asarray(test_set_x,
+                                                   dtype=theano.config.floatX),name = 'test', borrow = True)
+
+    # pick random test examples, with which to initialize the persistent chain
+    test_idx = rng.randint(number_of_test_samples - n_chains)
+    persistent_vis_chain = theano.shared(
+        np.asarray(
+            test_set_x.get_value(borrow=True)[test_idx:test_idx + n_chains],
+            dtype=theano.config.floatX
+        )
+    )
+    # end-snippet-6 start-snippet-7
+    plot_every = 1000
+    # define one step of Gibbs sampling (mf = mean-field) define a
+    # function that does `plot_every` steps before returning the
+    # sample for plotting
+    (
+        [
+            presig_hids,
+            hid_mfs,
+            hid_samples,
+            presig_vis,
+            vis_mfs,
+            vis_samples
+        ],
+        updates
+    ) = theano.scan(
+        mpf_optimizer.gibbs_vhv,
+        outputs_info=[None, None, None, None, None, persistent_vis_chain],
+        n_steps=plot_every
+    )
+
+    # add to updates the shared variable that takes care of our persistent
+    # chain :.
+    updates.update({persistent_vis_chain: vis_samples[-1]})
+    # construct the function that implements our persistent chain.
+    # we generate the "mean field" activations for plotting and the actual
+    # samples for reinitializing the state of our persistent chain
+    sample_fn = theano.function(
+        [],
+        [
+            vis_mfs[-1],
+            vis_samples[-1]
+        ],
+        updates=updates,
+        name='sample_fn'
+    )
+
+    # create a space to store the image for plotting ( we need to leave
+    # room for the tile_spacing as well)
+    image_data = np.zeros(
+        (29 * n_samples + 1, 29 * n_chains - 1),
+        dtype='uint8'
+    )
+    for idx in range(n_samples):
+        # generate `plot_every` intermediate samples that we discard,
+        # because successive samples in the chain are too correlated
+        vis_mf, vis_sample = sample_fn()
+        print(' ... plotting sample ', idx)
+        image_data[29 * idx:29 * idx + 28, :] = tile_raster_images(
+            X=vis_mf,
+            img_shape=(28, 28),
+            tile_shape=(1, n_chains),
+            tile_spacing=(1, 1)
+        )
+
+    # construct image
+    image = Image.fromarray(image_data)
+    image.save('samples.png')
+    # end-snippet-7
+    os.chdir('../')
+
+    return mpf_optimizer.W.get_value(borrow = True), mpf_optimizer.b.get_value(borrow = True)
+
+
+if __name__ == '__main__':
+
+    W, b = mpf_em(hidden_units=300, dataset= None)
+
+
+    test_image = tile_raster_images(
+        X=W.T,
+        img_shape=(28, 28),
+        tile_shape=(10, 10),
+        tile_spacing=(1, 1)
+    )
+    print(test_image.shape)
+    image = Image.fromarray(test_image)
+
+    image.save('filters.png')
+
+
+
+
+
+
---
+++ np_mpf.py
@@ -0,0 +1,301 @@
+import numpy as np
+import gzip
+import timeit, pickle, sys, math
+import matplotlib.pyplot as plt
+import sys
+from numpy.linalg import norm
+from deepMPF import get_mpf_params
+sys.setrecursionlimit(40000)
+from scipy.optimize import fmin_l_bfgs_b as minimize
+from SparseAutoencoder_minst import displayNetwork as display
+from sklearn import preprocessing
+
+
+def unravel_params(theta,visible_size):
+    W = theta[0:visible_size*visible_size].reshape(visible_size,visible_size)
+    b = theta[visible_size*visible_size:].reshape(visible_size,1)
+    return W,b
+
+def ravel_params(W,b):
+    return np.concatenate((W.ravel(),b.ravel()))
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+def normalizeData(patches):
+    # Squash data to [0.1, 0.9] since we use sigmoid as the activation
+    # function in the output layer
+
+    # Remove DC (mean of images).
+    patches = patches-np.array([np.mean(patches,axis=1)]).T
+
+    # Truncate to +/-3 standard deviations and scale to -1 to 1
+    pstd = 3*np.std(patches)
+    patches = np.fmax(np.fmin(patches,pstd),-pstd)/pstd
+
+    # Rescale from [-1,1] to [0.1,0.9]
+    patches = (patches+1)*0.4+0.1
+    return patches
+
+class np_dmpf_optimizer(object):
+
+    def __init__(self, vis_units, hid_units,epsilon, batch_sz = 20):
+
+        self.visible_units = vis_units
+        self.hidden_units = hid_units
+        self.epsilon = epsilon
+        self.batch_sz = batch_sz
+        self.num_neurons = self.visible_units + self.hidden_units
+        numpy_rng = np.random.RandomState(12336)
+        self.W = get_mpf_params(self.visible_units,self.hidden_units)
+        self.b = np.zeros(self.num_neurons)
+        self.zero_grad = None
+
+    def propup(self, data_samples):
+
+        activations =  sigmoid(np.dot(data_samples,self.W[:self.visible_units,self.visible_units:])
+                               +self.b[self.visible_units:].reshape([1,-1]))
+        samples = np.random.binomial(n=1,p= activations)
+        return [activations, samples]
+
+    def get_samples(self, theta, data_samples, n_samples = 5):
+
+        self.W, self.b = unravel_params(theta, visible_size= self.num_neurons)
+
+
+        if self.zero_grad is None:
+            a = np.ones((self.visible_units,self.hidden_units))
+            b = np.zeros((self.visible_units,self.visible_units))
+            c = np.zeros((self.hidden_units,self.hidden_units))
+            zero_grad_u = np.concatenate((b,a),axis = 1)
+            zero_grad_d = np.concatenate((a.T,c),axis=1)
+            self.zero_grad = np.concatenate((zero_grad_u,zero_grad_d),axis=0)
+
+
+        sample_prob = None
+        data = None
+        norm_sample_prob = None
+
+        for i in range(n_samples):
+            prop = self.propup(data_samples=data_samples)
+            activations = prop[0]
+            hidden_samples = prop[1]
+            new_data = np.concatenate((data_samples,hidden_samples),axis=1)
+
+            probality = (1 - hidden_samples) * (1-activations) + hidden_samples * activations
+            new_sample_prob = np.prod(probality, axis = 1)
+
+            if data is None:
+                data = new_data
+                sample_prob = new_sample_prob
+                norm_sample_prob = new_sample_prob
+
+            else:
+                data = np.concatenate((data,new_data),axis = 0)
+                sample_prob = np.concatenate((sample_prob,new_sample_prob))
+                norm_sample_prob += new_sample_prob
+
+        norm_sample_prob = np.tile(norm_sample_prob,reps=n_samples)
+        sample_prob = sample_prob / norm_sample_prob
+
+        return sample_prob, data
+
+
+    def get_cost_updates(self, theta, data, sample_prob):
+
+
+         # computing the cost and gradient
+        self.W, self.b = unravel_params(theta, visible_size= self.num_neurons)
+
+
+        self.batch_sz = data.shape[0]
+
+
+        z = 1/2 - data
+        energy_difference = z * (np.dot(data,self.W)+ self.b.reshape([1,-1]))
+
+        sample_prob = sample_prob.reshape((1,-1)).T
+        k = np.asarray(np.ones(self.num_neurons)).reshape([1,-1])
+        sample_prob = np.dot(sample_prob, k)
+
+        cost = (self.epsilon/self.batch_sz) * np.sum(np.exp(energy_difference)* sample_prob)
+
+        # computing the gradient
+        h = z * np.exp(energy_difference) * sample_prob
+
+        bgrad = np.mean(h,axis=0)*self.epsilon
+        Wgrad = (np.dot(h.T,data)+np.dot(data.T,h))*self.epsilon/self.batch_sz
+
+        #np.fill_diagonal(Wgrad,0)
+        Wgrad *= self.zero_grad
+
+        grad = ravel_params(Wgrad,bgrad)
+
+        return cost, grad
+
+
+def computeNumericalGradient(J,theta):
+    # numgrad = computeNumericalGradient(J, theta)
+    # theta: a vector of parameters
+    # J: a function that outputs r.
+    # Calling y = J(theta) will return the function value at theta.
+
+    # Initialize numgrad with zeros
+    numgrad = np.zeros(np.shape(theta))
+
+    ## ---------- YOUR CODE HERE --------------------------------------
+    # Instructions:
+    # Implement numerical gradient checking, and return the result in numgrad.
+    # (See Section 2.3 of the lecture notes.)
+    # You should write code so that numgrad(i) is (the numerical approximation to) the
+    # partial derivative of J with respect to the i-th input argument, evaluated at theta.
+    # I.e., numgrad(i) should be the (approximately) the partial derivative of J with
+    # respect to theta(i).
+    #
+    # Hint: You will probably want to compute the elements of numgrad one at a time.
+    for i in range(0,numgrad.shape[0]):
+        k = np.zeros(np.shape(theta))
+        k[i] = 0.0001
+        epsW,epsb = unravel_params(k,20)
+        epsW = epsW+epsW.T
+        k = ravel_params(epsW,epsb)
+        y1 = J(theta + k)
+        y2 = J(theta - k)
+        numgrad[i] = (y1-y2)/0.0002
+
+    return numgrad
+
+
+def train_bfgs_rbm(epsilon,n_samples,epoches):
+
+    vis_units = 64
+    hid_units = 25
+    epsilon = epsilon
+    learning_rate = 0.01
+    n_samples = n_samples
+
+    # W = np.load('rbm_weight_10000.npy')
+    # #print(W.shape)
+    #
+    # rbm_data = np.load('rbm_samples_10000.npy')
+    # print(rbm_data.shape)
+
+    Images = np.load('IMAGES.npy')
+    num_patches = 10000
+    patch_size = 8
+    patches = np.zeros((num_patches,patch_size*patch_size))
+
+    for i in range(num_patches):
+        pix_index = np.random.randint(504,size=2)
+        im_index = np.random.randint(10)
+        patches[i,:] = patches[i,:] + \
+                       Images[pix_index[0]:pix_index[0]+8,pix_index[1]:pix_index[1]+8,im_index].reshape(1,64)
+
+
+    patches = normalizeData(patches)
+    #display(patches[:100,:])
+
+    binarizer = preprocessing.Binarizer(threshold=0.5)
+    patches =  binarizer.transform(patches)
+
+    print(patches[0,:])
+
+    mpf_optimizer = np_dmpf_optimizer(vis_units = vis_units, hid_units= hid_units, epsilon = epsilon)
+
+
+    ###### Check Gradient ######
+    theta = ravel_params(mpf_optimizer.W, mpf_optimizer.b)
+
+    sample_prob, data = mpf_optimizer.get_samples(theta=theta, data_samples = patches, n_samples = n_samples)
+
+
+    #numpy_rng = np.random.RandomState(555555)
+    # W = numpy_rng.randn(20,20)/np.sqrt(20*20)
+    # W = (W + W.T)/2
+    # np.fill_diagonal(W,0)
+    # b = np.zeros((20,1))
+    # theta = ravel_params(W, b)
+    # numgrad = computeNumericalGradient(lambda x: mpf_optimizer.get_cost_updates(
+    #     x, data, sample_prob)[0],theta)
+    # cost, grad = mpf_optimizer.get_cost_updates(theta,data, sample_prob)
+    # # Use this to visually compare the gradients side by side
+    # print(np.array([numgrad,grad]).T)
+    # # Compare numerically computed gradients with the ones obtained from backpropagation
+    # diff = norm(numgrad-grad)/norm(numgrad+grad)
+    # print(diff)
+    # print('diff is computed')
+
+
+    ######  Run the MPF ######
+
+    ## We first try the MPF with l-bfgs algorithms
+
+    opttheta,cost,messages = minimize(mpf_optimizer.get_cost_updates,theta,fprime=None,maxiter=400,
+                                     args=(data, sample_prob))
+
+    for i in range(epoches):
+        theta = opttheta
+        sample_prob, data = mpf_optimizer.get_samples(theta= theta, data_samples = patches, n_samples=n_samples)
+        opttheta,cost,messages = minimize(mpf_optimizer.get_cost_updates,theta,fprime=None,maxiter=400,
+                                         args=(data, sample_prob))
+
+        W1,b1 = unravel_params(opttheta,visible_size= vis_units+hid_units)
+        W1 = W1[:vis_units,vis_units:]
+        saveName = 'weights_' + str(n_samples) + '_' + str(epsilon) + '_' + str(epoches) + '.png'
+        display(W1.T,saveName=saveName)
+
+
+
+
+    ######################## Run MPF with SGD ###############################
+
+
+    ##########################################################################
+
+
+
+    # W_prime, b_prime = unravel_params(opttheta,visible_size= 20)
+    #
+    # W_prime  = W_prime[:vis_units,vis_units:]
+    #
+    # fig1 = plt.figure()
+    # ax1 = fig1.add_subplot(111)
+    # ax1.set_title('SGD Diff between W and W_prime')
+    # plt.imshow(np.abs(W - W_prime), extent=[0,10,0,10],aspect = 'auto')
+    # plt.colorbar()
+    # plt.show()
+    # fig1.savefig('np_mpf_Sgd_Imageseq.png')
+    #
+    # W1 = W_prime.ravel()
+    # W2 = W.ravel()
+    # #
+    # # index = np.random.random_integers(low=0,high=300,size = (100,))
+    # # W1 = W1[index]
+    # # W2 = W2[index]
+    #
+    # print(np.mean(np.sqrt((W-W_prime)**2)))
+    #
+    # fig1 = plt.figure()
+    # ax1 = fig1.add_subplot(111)
+    # ax1.set_title('SGD: Diff of Randomly 100 Weight')
+    # plt.plot(W1,'y')
+    # plt.plot(W2,'c')
+    # plt.legend(['Recover W', 'Original W'])
+    # plt.show()
+    # fig1.savefig('np_mpf_Random_Diff.png')
+    # plt.close()
+
+
+
+if __name__ == '__main__':
+
+
+
+
+
+
+
+
+
